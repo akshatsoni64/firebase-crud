4d553cd6b1e01aa9af318f46bdf37019eb0e0d4e
akshatsoni64 <akshatsoni64@gmail.com>
2021-04-04 23:57:35 +0530

    Update & Delete Incomplete

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from firebase_admin import firestore, credentials, initialize_app
 
 # Initialize the app
+# GET YOU CRED FILE FROM SERVICE ACCOUNTS INSIDE YOUR FIREBASE DB PROJECT'S CONSOLE
 cred_obj = credentials.Certificate("cred/cred.json")
 myapp = initialize_app(cred_obj)
 
